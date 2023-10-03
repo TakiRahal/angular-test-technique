29aafcd19eefaed5000002f94c632a849473d692
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2023-10-03 12:10:55 +0100

    calculate amount from eur to usd

--- src/app/components/currency-form/currency-form.component.ts
+++ src/app/components/currency-form/currency-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CurrencyService} from "../../services/currency.service";
 import {Subscription} from "rxjs";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-currency-form',
@@ -9,17 +10,49 @@ import {Subscription} from "rxjs";
 })
 export class CurrencyFormComponent  implements OnInit, OnDestroy{
 
-  valueExchange = 0;
-  subscription: Subscription = new Subscription();
+    valueExchange = 0;
+    subscription: Subscription = new Subscription();
 
-  constructor(private currencyService: CurrencyService) {}
-  ngOnInit(): void {
-    this.subscription = this.currencyService.exchangeRate$
-        .subscribe(input => {
-          console.log('input ', input)
-          this.valueExchange = input;
+    formGroup!: FormGroup;
+
+    submitted = false;
+
+    resultCalculate!: number;
+
+    constructor(private fb: FormBuilder,
+                private currencyService: CurrencyService) {}
+    ngOnInit(): void {
+
+        // Initialisation form
+        this.initForm();
+
+        this.subscription = this.currencyService.exchangeRate$
+            .subscribe(input => {
+              console.log('input ', input)
+              this.valueExchange = input;
+                this.calculateExchange(input);
+            })
+    }
+
+    /**
+     * For initialisation form
+     */
+    private initForm(): void{
+        this.formGroup = this.fb.group({
+            amount: new FormControl('', Validators.required),
         })
-  }
-  ngOnDestroy(): void {
-  }
+    }
+
+    calculateExchange(exchangeValue: number): void{
+        this.submitted = true;
+        if(this.formGroup.valid) {
+            const resultTmp = +this.formGroup.get('amount')?.value * exchangeValue;
+            this.resultCalculate = +resultTmp.toFixed(2);
+            console.log('this.resultCalculate ', this.resultCalculate)
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
